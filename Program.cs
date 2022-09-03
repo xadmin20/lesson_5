2c9678ab21350a466f9bafc91ea29934a3f8b3df
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-03 17:40:51 +0300

    Update Program.cs
    Задача №38

--- Program.cs
+++ Program.cs
@@ -1,24 +1,35 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
 void answer(int B){
-    int[] arr = rnd_num(B); 
-    int sum = 0;
-    for (int i = 0; i < arr.Length; i++) if (i % 2 != 0) sum = sum + arr[i];
-    Console.Write("[");
-    for (int i = 0; i < B - 1; ++i) Console.Write(arr[i] + ", ");
-    Console.WriteLine($"{arr[B - 1]}] -> {sum}");
+    int  i = 0;
+    double[] arr = rnd_num_d(B);
+    var min = arr[0];
+    var max = arr[0];
+    var minIndex = 0;
+    var maxIndex = 0;
+    for (i = 0; i < B; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            minIndex = i;
+        }
+        else if (arr[i] > max)
+        {
+            max = arr[i];
+            maxIndex = i;
+        }
 }
-//функция создает массив и возвращает ёё
-int[] rnd_num(int A){
-    int[] arr = new int[A];
+    Console.Write("[");
+    for (int x = 0; x < B - 1; ++x) Console.Write(arr[x] + " ");
+    Console.WriteLine($"{arr[B - 1]}] -> {max-min}");
+}
+double[] rnd_num_d(int A){
+    double[] arr = new double[A];
     Random rnd = new Random();
     for (int i = 0; i < A; i++) arr[i] = rnd.Next(-100, 100);
     return arr;
 }
-//отправляем запрос на размер массива
 Console.WriteLine("Введите размер массива: ");
 int B = Convert.ToInt32(Console.ReadLine());
-//запускаем главную функцию
 answer(B);
-            
