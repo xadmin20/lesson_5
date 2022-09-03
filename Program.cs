c516d6f9905b50ec60222f81dfbb9e8c38ae15bb
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-03 16:29:36 +0300

    Update Program.cs
    "Задача 36"

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 void answer(int B){
     int[] arr = rnd_num(B); 
-    int k = 0;
-    foreach(int element in arr) if (element % 2 == 0) k++;
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++) if (i % 2 != 0) sum = sum + arr[i];
     Console.Write("[");
     for (int i = 0; i < B - 1; ++i) Console.Write(arr[i] + ", ");
-    Console.WriteLine(arr[B - 1] + "] -> Четных элементов: " + k);
+    Console.WriteLine($"{arr[B - 1]}] -> {sum}");
 }
 //функция создает массив и возвращает ёё
 int[] rnd_num(int A){
     int[] arr = new int[A];
     Random rnd = new Random();
-    for (int i = 0; i < A; i++) arr[i] = rnd.Next(100, 999);
+    for (int i = 0; i < A; i++) arr[i] = rnd.Next(-100, 100);
     return arr;
 }
 //отправляем запрос на размер массива
@@ -21,3 +21,4 @@ Console.WriteLine("Введите размер массива: ");
 int B = Convert.ToInt32(Console.ReadLine());
 //запускаем главную функцию
 answer(B);
+            
